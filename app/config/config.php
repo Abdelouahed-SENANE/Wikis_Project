ccd8254fa38f75ed3663c29357534edc741ff0bd
AbdelouhedSN <abdosenane75@gmail.com>
2024-01-08 15:25:34 +0100

    Create Wikis Project

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+// Database Params 
+define('DB_HOST' , 'localhost');
+define('DB_USER' , 'root');
+define('DB_PASS' , '');
+define('DB_NAME' , 'db_wikis');
+// App Root
+define('APPROOT' , dirname(dirname(__FILE__)));
+
+// App URL
+define('URLROOT' , '_YOUR_URL_'); //http://localhost/FILEPROJECT 
+// SiteName
+define('SITENAME' , '_YOUR_SITENAME_');
+?>
